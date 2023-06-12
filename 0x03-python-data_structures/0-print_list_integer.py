c9a92621a7144a51dd9d8f78e90ee3ad4ed560ac
Bouchra-e <essadanybouchra71@gmail.com>
2023-06-12 21:19:38 +0000

    update

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -3,7 +3,3 @@
 def print_list_integer(my_list=[]):
     for num in my_list:
         print("{:d}".format(num))
-
-if __name__ == "__main__":
-    import sys
-    print_list_integer(eval(sys.argv[1]))
